df7b323736867a5feed36608575e3a0950f4e8fd
ManishatRedHat <manishsaxena2007@gmail.com>
2015-09-09 19:44:49 -0400

    Changed the HTML for the site

--- mysite_adv/urls.py
+++ mysite_adv/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite_adv.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
